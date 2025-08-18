6af4df3ba66ff033428e0e2aa906be10dd71952f
jplegeratma <jp.leger@mass.gov>
2025-08-18 11:08:04 -0400

    Sub_Scorecard_5-4.sql 4/17/25

--- Sub_Scorecard_5.sql
+++ Sub_Scorecard_5.sql
@@ -1,9 +1,18 @@
 -- 5.3
 
-CREATE TABLE WH_COUNTS_5_3
-AS
+-------------
 
--- look for 8 month back 
+-- 4/17/25
+-- Informatica M_INF_B_SUB_DASH_KPI_COUNTS_5_3
+
+CREATE TABLE INF_B_SUB_DASH_KPI_COUNTS_5_3
+AS
+SELECT * FROM WH_COUNTS_5_3;
+
+TRUNCATE TABLE INF_B_SUB_DASH_KPI_COUNTS_5_3;
+
+INSERT INTO INF_B_SUB_DASH_KPI_COUNTS_5_3
+
 SELECT 
 RUN_DATE, 
 CDE_ENTITY_MODEL, 
@@ -12,7 +21,8 @@ ENTITY_NAME,
 CLAIM_MCE, 
 CLAIM_ACO_MCE, 
 WH_MON, 
-SUM(TOTAL_RECS) AS EIGHT_MON_RECS
+SUM(TOTAL_RECS) AS EIGHT_MON_RECS,
+SUM(TOTAL_PAID) AS EIGHT_MON_PAID
 FROM (
 
 -- sum DOS records
@@ -25,7 +35,8 @@ CLAIM_MCE,
 CLAIM_ACO_MCE, 
 WH_MON, 
 DOS_MON, 
-COUNT(ENC_CLAIM_NO||ENC_CLAIM_SUFFIX) AS TOTAL_RECS
+COUNT(ENC_CLAIM_NO||ENC_CLAIM_SUFFIX) AS TOTAL_RECS,
+SUM(AMT_PAID) AS TOTAL_PAID
 FROM (
 
 -- core 
@@ -41,7 +52,88 @@ CURRENT_DATE() AS RUN_DATE,
                  ENC_CLAIM_NO,
                  ENC_CLAIM_SUFFIX,
                  TO_CHAR(DOS_FROM_DT,'YYMM') AS DOS_MON,
-                 TO_CHAR(WH_FROM_DT,'YYMM') AS WH_MON
+                 TO_CHAR(WH_FROM_DT,'YYMM') AS WH_MON,
+                 AMT_PAID
+                 FROM mhdwprod.nw.nw_encounter_hist e
+         left join mhteam.dwdq.INF_B_MCE_PIDSL_CROSSWALK cw on cw.mco = e.cde_enc_mco and cw.aco = e.cde_enc_aco            
+         WHERE e.dos_from_dt >= '01-JAN-2022'
+         AND e.IND_OFFSET = 'N'
+--and E.CDE_ENC_ACO = 'BMC-BACO'
+
+)
+GROUP BY RUN_DATE, 
+         CDE_ENTITY_MODEL, 
+         ENTITY_PIDSL, 
+         ENTITY_NAME, 
+         CLAIM_MCE, 
+         CLAIM_ACO_MCE, 
+         WH_MON, 
+         DOS_MON
+HAVING DOS_MON < TO_CHAR(DATEADD(month, -7, TO_DATE(WH_MON,'YYMM')),'YYMM')
+
+ORDER BY RUN_DATE, CDE_ENTITY_MODEL, ENTITY_PIDSL, ENTITY_NAME, CLAIM_MCE, CLAIM_ACO_MCE, WH_MON DESC , DOS_MON DESC
+
+)
+GROUP BY RUN_DATE, 
+         CDE_ENTITY_MODEL, 
+         ENTITY_PIDSL, 
+         ENTITY_NAME, 
+         CLAIM_MCE, 
+         CLAIM_ACO_MCE, 
+         WH_MON
+
+ORDER BY RUN_DATE, CDE_ENTITY_MODEL, ENTITY_PIDSL, ENTITY_NAME, CLAIM_MCE, CLAIM_ACO_MCE, WH_MON DESC
+;
+
+-------------
+
+--DROP TABLE WH_COUNTS_5_3;
+
+CREATE TABLE WH_COUNTS_5_3
+AS
+
+-- look for 8 month back 
+SELECT 
+RUN_DATE, 
+CDE_ENTITY_MODEL, 
+ENTITY_PIDSL, 
+ENTITY_NAME, 
+CLAIM_MCE, 
+CLAIM_ACO_MCE, 
+WH_MON, 
+SUM(TOTAL_RECS) AS EIGHT_MON_RECS,
+SUM(TOTAL_PAID) AS EIGHT_MON_PAID
+FROM (
+
+-- sum DOS records
+SELECT 
+RUN_DATE, 
+CDE_ENTITY_MODEL, 
+ENTITY_PIDSL, 
+ENTITY_NAME, 
+CLAIM_MCE, 
+CLAIM_ACO_MCE, 
+WH_MON, 
+DOS_MON, 
+COUNT(ENC_CLAIM_NO||ENC_CLAIM_SUFFIX) AS TOTAL_RECS,
+SUM(AMT_PAID) AS TOTAL_PAID
+FROM (
+
+-- core 
+SELECT
+--TO_DATE('20250225','YYYYMMDD') AS RUN_DATE,
+CURRENT_DATE() AS RUN_DATE,
+                 cw.CDE_ENTITY_MODEL, 
+                 cw.ENTITY_PIDSL,
+                 cw.ENTITY_NAME,
+                 E.CDE_ENC_MCO AS CLAIM_MCE,
+                 CASE WHEN E.CDE_ENC_ACO in('#','+','-') 
+                     THEN E.CDE_ENC_MCO ELSE E.CDE_ENC_ACO END AS CLAIM_ACO_MCE,
+                 ENC_CLAIM_NO,
+                 ENC_CLAIM_SUFFIX,
+                 TO_CHAR(DOS_FROM_DT,'YYMM') AS DOS_MON,
+                 TO_CHAR(WH_FROM_DT,'YYMM') AS WH_MON,
+                 AMT_PAID
                  FROM mhdwprod.nw.nw_encounter_hist e
          left join mhteam.dwdq.INF_B_MCE_PIDSL_CROSSWALK cw on cw.mco = e.cde_enc_mco and cw.aco = e.cde_enc_aco            
          WHERE e.dos_from_dt >= '01-JAN-2022'
